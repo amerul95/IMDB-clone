27f892abd7ddef69d5bb59274ec54177af67f3c7
amerul95 <120275262+amerul95@users.noreply.github.com>
2024-06-07 17:35:55 +0800

    create about page

---
+++ src/app/about/page.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt enim ullam qui quisquam, numquam corporis harum ipsum nesciunt sed, beatae, quia suscipit voluptatibus soluta voluptatum quibusdam doloremque obcaecati. Esse recusandae repellendus, eligendi labore ratione illum blanditiis aspernatur odio dignissimos nihil vitae eaque, sapiente reiciendis voluptatum? Id ratione quod eligendi dolorum. Alias ipsa molestiae ut reiciendis tempore cupiditate quae, inventore quia. Dignissimos suscipit quod ea fugiat repudiandae consequuntur aspernatur, consectetur dolor illum, perferendis recusandae. Tempore, similique eligendi vero inventore non, cumque magnam, quibusdam eum placeat repellat excepturi earum in voluptate atque. Id explicabo tenetur nobis magni tempore minima repellendus earum molestiae!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt enim ullam qui quisquam, numquam corporis harum ipsum nesciunt sed, beatae, quia suscipit voluptatibus soluta voluptatum quibusdam doloremque obcaecati. Esse recusandae repellendus, eligendi labore ratione illum blanditiis aspernatur odio dignissimos nihil vitae eaque, sapiente reiciendis voluptatum? Id ratione quod eligendi dolorum. Alias ipsa molestiae ut reiciendis tempore cupiditate quae, inventore quia. Dignissimos suscipit quod ea fugiat repudiandae consequuntur aspernatur, consectetur dolor illum, perferendis recusandae. Tempore, similique eligendi vero inventore non, cumque magnam, quibusdam eum placeat repellat excepturi earum in voluptate atque. Id explicabo tenetur nobis magni tempore minima repellendus earum molestiae!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt enim ullam qui quisquam, numquam corporis harum ipsum nesciunt sed, beatae, quia suscipit voluptatibus soluta voluptatum quibusdam doloremque obcaecati. Esse recusandae repellendus, eligendi labore ratione illum blanditiis aspernatur odio dignissimos nihil vitae eaque, sapiente reiciendis voluptatum? Id ratione quod eligendi dolorum. Alias ipsa molestiae ut reiciendis tempore cupiditate quae, inventore quia. Dignissimos suscipit quod ea fugiat repudiandae consequuntur aspernatur, consectetur dolor illum, perferendis recusandae. Tempore, similique eligendi vero inventore non, cumque magnam, quibusdam eum placeat repellat excepturi earum in voluptate atque. Id explicabo tenetur nobis magni tempore minima repellendus earum molestiae!
+        </p>
+    </div>
+  )
+}
+
+export default About
+
