7bb6f210ce34aea02f014ee95b39910de9b18c79
amerul95 <120275262+amerul95@users.noreply.github.com>
2024-06-10 17:39:12 +0800

    create navbar compnents

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavbarItems from './NavbarItems'
+
+export default function Navbar() {
+  return (
+    <div className='flex dark:bg-gray-600 bg-amber-300 p-4 lg:text-lg justify-center gap-6'> 
+        <NavbarItems title="Trending" param="fetchTrending"/>
+        <NavbarItems title="Top Rated" param="fetchTopRated"/>
+    </div>
+  )
+}
