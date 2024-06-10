9ae5038a4d768bd4c0cac01b5d14eec2bd8db73c
amerul95 <120275262+amerul95@users.noreply.github.com>
2024-06-10 17:58:30 +0800

    small update for fetching API

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,12 @@
 import React from 'react'
+const API_KEY = process.env.API_KEY
 
-export default function page() {
+export default async function page({searchParams}) {
+  const genre = searchParams.genre || 'fetchTrending'
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+    genre === 'fetchTopRated' ? `/movie/top_rated`:`/trending/all/week`}?api_key=${API_KEY}`
+  )
   return (
     <div>page</div>
   )
